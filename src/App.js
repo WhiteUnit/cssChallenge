39281c833caaa77d8a562d4231fd3e502280fb5c
Filip Łasica <white@MacBook-Pro-Filip.local>
2021-09-14 11:54:24 +0200

    Day 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Day1 from './components/Day1';
+
+function App() {
+  return (
+    <div>
+      <Day1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Day1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Day1.css";
+
+const Day1 = () => {
+  return (
+    <div className="container">
+      <div className="digit-container">
+        <div className="first-digit top"></div>
+        <div className="first-digit bottom"></div>
+        <div className="first-zero"></div>
+        <div className="second-zero"></div>
+      </div>
+      <div className="big-word">DAYS</div>
+      <div className="small-word">CSS CHALLENGE</div>
+    </div>
+  );
+};
+
+export default Day1;

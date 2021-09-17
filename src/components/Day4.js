0ba7d343cce91931c467d28a8da13ad6bbcb0477
Filip Łasica <white@MacBook-Pro-Filip.local>
2021-09-17 13:04:56 +0200

    Day4

---
+++ src/components/Day4.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Day4.css";
+
+const Day4 = () => {
+  return (
+    <div className="frame-rings">
+      <div className="center-rings">
+        <div className="first-ring"></div>
+        <div className="second-ring"></div>
+        <div className="third-ring"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Day4;
